4b0d7b4e3ab378b5f1e5a11c02031d415b5c8c93
Hugo Januario <128010237+hugaojanuario@users.noreply.github.com>
2026-05-06 22:32:38 -0300

    refactor: refactor pkgs structure

--- internal/channel/channel_config.go
+++ internal/channel/channel_config.go
@@ -1,4 +1,4 @@
-package domain
+package channel
 
 import (
 	"time"
--- internal/notification/notification_log.go
+++ internal/notification/notification_log.go
@@ -4,7 +4,7 @@ import (
 	"time"
 
 	"github.com/google/uuid"
-	"github.com/hugaojanuario/NotifyGo/internal/domain"
+	"github.com/hugaojanuario/NotifyGo/internal/channel"
 )
 
 type NotificationStatus string
@@ -16,16 +16,16 @@ const (
 )
 
 type NotificationLog struct {
-	ID              uuid.UUID          `json:"id"`
-	RouteID         uuid.UUID          `json:"route_id"`
-	ChannelConfigID uuid.UUID          `json:"channel_config_id"`
-	Topic     string             `json:"topic"`
-	Channel   domain.ChannelType `json:"channel_type"`
-	Recipient string             `json:"recipient"`
-	Status    NotificationStatus `json:"status"`
-	Payload   string             `json:"payload"`
-	ErrorMessage    *string            `json:"error_message"`
-	Attempts        int                `json:"attempts"`
-	SentAt          *time.Time         `json:"sent_at"`
-	CreatedAt       time.Time          `json:"created_at"`
+	ID              uuid.UUID           `json:"id"`
+	RouteID         uuid.UUID           `json:"route_id"`
+	ChannelConfigID uuid.UUID           `json:"channel_config_id"`
+	Topic           string              `json:"topic"`
+	Channel         channel.ChannelType `json:"channel_type"`
+	Recipient       string              `json:"recipient"`
+	Status          NotificationStatus  `json:"status"`
+	Payload         string              `json:"payload"`
+	ErrorMessage    *string             `json:"error_message"`
+	Attempts        int                 `json:"attempts"`
+	SentAt          *time.Time          `json:"sent_at"`
+	CreatedAt       time.Time           `json:"created_at"`
 }
